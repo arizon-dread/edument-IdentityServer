9a0daf20a87fc2fc3de2e5f7f05ad6983cd5a40f
Erik Svensson <erik.svensson@regionorebrolan.se>
2021-01-26 11:27:58 +0100

    Added identityServer stuff.

--- IdentityService/Startup.cs
+++ IdentityService/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServerHost.Quickstart.UI;
+using IdentityServerInMem;
 using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -32,6 +34,16 @@ namespace IdentityService
                 opts.IncludeSubDomains = true;
                 opts.MaxAge = TimeSpan.FromSeconds(15768000);
             });
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer(options =>
+            {
+
+            }).AddTestUsers(TestUsers.Users)
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients);
+            builder.AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,34 +57,46 @@ namespace IdentityService
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
-            app.UseRouting();
             app.UseSecurityHeaders();
+
+            app.UseRouting();
+           
             app.UseRequestLocalization(
                 new RequestLocalizationOptions()
                 .SetDefaultCulture("se-SE"));
 
+            app.UseStaticFiles();
+
+            app.UseIdentityServer();
+
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    string build = "Debug build";
-                    if (Settings.IsReleaseBuild)
-                    {
-                        build = "Release build";
-                    }
-
-                    //Print out the first 8 characters of the GitHub SHA when deploying to production
-                    //Should of course be a bit more hidden in real life, perhaps as a HTML comment?
-                    var gitHubSha = _configuration["GITHUB:SHA"] ?? "";
-                    if (gitHubSha.Length > 8)
-                    {
-                        gitHubSha = " " + gitHubSha.Substring(0, 8);
-                    }
-
-                    //In real life this Should of course be a bit more hidden, perhaps as a HTML comment?
-                    await context.Response.WriteAsync($"Hello Identity Service!, Deployed {Infrastructure.Settings.StartupTime} ({env.EnvironmentName}, {build}{gitHubSha})");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        string build = "Debug build";
+            //        if (Settings.IsReleaseBuild)
+            //        {
+            //            build = "Release build";
+            //        }
+
+            //        //Print out the first 8 characters of the GitHub SHA when deploying to production
+            //        //Should of course be a bit more hidden in real life, perhaps as a HTML comment?
+            //        var gitHubSha = _configuration["GITHUB:SHA"] ?? "";
+            //        if (gitHubSha.Length > 8)
+            //        {
+            //            gitHubSha = " " + gitHubSha.Substring(0, 8);
+            //        }
+
+            //        //In real life this Should of course be a bit more hidden, perhaps as a HTML comment?
+            //        await context.Response.WriteAsync($"Hello Identity Service!, Deployed {Infrastructure.Settings.StartupTime} ({env.EnvironmentName}, {build}{gitHubSha})");
+            //    });
+            //});
         }
     }
 }
